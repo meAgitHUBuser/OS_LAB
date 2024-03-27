446c4a7c0bae67af5d723292e48aae48b11dfeb3
meAgitHUBuser <163905570+meAgitHUBuser@users.noreply.github.com>
2024-03-27 15:14:24 +0530

    Create pgm5.c

---
+++ pgm5.c
@@ -0,0 +1,46 @@
+// Bankers 
+
+#include<stdio.h>
+#define max 10
+int main(){
+    int n, avail, temp[max], time[max];
+    char job[max][10];
+    printf("Enter the number of jobs: ");
+    scanf("%d",&n);
+    printf("Enter the job name and processing time: \n");
+    for(int i=0;i<n;i++){
+        scanf("%s%d",job[i],&time[i]);
+        temp[i]=i;
+    }
+    printf("Enter the available resources: ");
+    scanf("%d",&avail);
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(time[j]>time[j+1]){
+                int tem = time[j];
+                time[j] = time[j+1];
+                time[j+1] = tem;
+                
+                tem = temp[j];
+                temp[j] = temp[j+1];
+                temp[j+1] = tem;
+            }
+        }
+    }
+    
+    printf("Safe sequence is: ");
+    int total_time=0;
+    for(int i=0;i<n;i++){
+        int q = temp[i];
+        if(time[q]<=avail){
+            printf("%s ",job[q]);
+            avail -= time[q];
+            total_time += time[q];
+        }
+        else{
+            printf("\nNot enough resources for further jobs");
+            return 0;
+        }
+    }
+    return 0;
+}
